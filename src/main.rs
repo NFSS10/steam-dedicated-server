cde4220cb8ba235b4de5e4d849ba4f21f4bc5943
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-02 23:06:56 +0000

    Verify if projects requirements are set before running

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@
 mod games;
 mod utils;
 
+use std::error::Error;
+use std::process::Command;
+
 use games::cs2::menu;
 use menu_rs::{Menu, MenuOption};
+use utils::paths::get_steamcmd_exe_path;
 
 fn main() {
+    // Checks if SteamCMD is set up correctly
+    match verify_os() {
+        Ok(_) => {}
+        Err(_) => println!("Setup required. Please ensure that SteamCMD is correctly set up."),
+    }
+
     games_menu();
 }
 
@@ -15,4 +25,17 @@ fn games_menu() {
         MenuOption::new("Counter Strike 2", menu).hint("Runs the server")
     ]);
     menu.show();
+}
+
+fn verify_os() -> Result<(), Box<dyn Error>> {
+    if cfg!(windows) {
+        let steacmd_exe_path = get_steamcmd_exe_path()?;
+        if !steacmd_exe_path.exists() {
+            Err("steamcmd.exe not found")?;
+        }
+    } else if cfg!(unix) {
+        Command::new("steamcmd").arg("+quit").output()?;
+    }
+
+    Ok(())
 }
