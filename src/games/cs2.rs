9703c521e7a0d0bd18029808e2e91f073add1bf4
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-02 23:32:15 +0000

    Implement CS2 install process

--- src/games/cs2.rs
+++ src/games/cs2.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
+use std::process::Command;
+
 use menu_rs::{Menu, MenuOption};
 
-use crate::utils::paths::get_app_dir_path;
+use crate::utils::paths::{get_app_dir_path, get_steamcmd_exe_path};
 
 pub fn menu() {
     let menu = Menu::new(vec![
@@ -32,14 +35,29 @@ enum GameMode {
 }
 
 fn start_server(mode: GameMode) {
-    // TODO
+    if let Err(err) = _start_server(mode) {
+        eprintln!("Error running server: {}", err);
+    }
 }
 
 fn install_server() {
+    if let Err(err) = _install_server() {
+        eprintln!("Error installing server: {}", err);
+    }
+}
+
+fn _start_server(mode: GameMode) -> Result<(), Box<dyn Error>> {
+    // TODO
+
+    Ok(())
+}
+
+fn _install_server() -> Result<(), Box<dyn Error>> {
     // builds path to the CS2 server folder
     let mut server_dir_path = get_app_dir_path().expect("Failed to get the application path");
     server_dir_path.push("cs2-server");
 
+    // builds install arguments
     let args = [
         &format!("+force_install_dir {}", server_dir_path.to_str().unwrap()),
         "+login anonymous",
@@ -47,6 +65,13 @@ fn install_server() {
         "+quit",
     ];
 
-    println!("Installing CS2 server...");
-    println!("TODO: {:?}", args);
+    // runs install command
+    if cfg!(windows) {
+        let steacmd_exe_path = get_steamcmd_exe_path()?;
+        Command::new(steacmd_exe_path).args(args).status()?;
+    } else if cfg!(unix) {
+        Command::new("steamcmd").args(args).status()?;
+    }
+
+    Ok(())
 }
