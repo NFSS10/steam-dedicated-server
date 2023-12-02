7bb4e809a0b1ec1ccc040873f02e44b657babb21
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-02 23:07:59 +0000

    Don't immediately close in windows

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod games;
 mod utils;
 
 use std::error::Error;
+use std::io::stdin;
 use std::process::Command;
 
 use games::cs2::menu;
@@ -11,13 +12,19 @@ use menu_rs::{Menu, MenuOption};
 use utils::paths::get_steamcmd_exe_path;
 
 fn main() {
-    // Checks if SteamCMD is set up correctly
+    // checks if SteamCMD is set up correctly
     match verify_os() {
         Ok(_) => {}
         Err(_) => println!("Setup required. Please ensure that SteamCMD is correctly set up."),
     }
 
     games_menu();
+
+    // avoids immediately closing the console in Windows
+    if cfg!(windows) {
+        println!("\nPress any key to close");
+        stdin().read_line(&mut String::new()).unwrap();
+    }
 }
 
 fn games_menu() {
