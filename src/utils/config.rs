43a8916e883d671a1648f7f83ff7e2113f305bf8
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-03 20:46:49 +0000

    Create `load_cfg()`

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -39,6 +39,39 @@ pub fn load_commands(file_path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(entries)
 }
 
+pub fn load_cfg(file_path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
+    let reader = match load_file(&file_path) {
+        Ok(r) => r,
+        Err(_) => {
+            eprintln!("Can't load {:?}", file_path);
+            Err("Error loading .cfg")?
+        }
+    };
+
+    let mut entries = Vec::new();
+    for line in reader.lines() {
+        let mut line = line?;
+        line = line.trim().to_owned();
+
+        let ignore_line = line.starts_with('/') || line.is_empty();
+        if ignore_line {
+            continue;
+        }
+
+        // removes extra white spaces between words
+        let words: Vec<_> = line.split_whitespace().collect();
+        line = words.join(" ");
+
+        // removes comments
+        let words: Vec<_> = line.split(" //").collect();
+        line = words[0].to_owned();
+
+        entries.push(line);
+    }
+
+    Ok(entries)
+}
+
 fn load_file(file_path: &Path) -> Result<BufReader<File>, Box<(dyn Error + 'static)>> {
     if !file_path.exists() {
         Err("Error loading file")?;
